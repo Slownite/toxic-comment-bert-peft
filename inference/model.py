b9537c3d585e17ba29e41a84d2a9555bbb0b42f4
Samuel Diop <snfdiop@outlook.com>
2025-05-20 14:16:03 +0200

    retrain model on all dataset

---
+++ inference/model.py
@@ -0,0 +1,23 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from peft import PeftModel
+import torch
+def initialize_model():
+    base = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=1)
+    model = PeftModel.from_pretrained(base, "model/final")
+    tokenizer = AutoTokenizer.from_pretrained("model/final", local_files_only=True)
+    return model.eval(), tokenizer
+
+model, tokenizer = initialize_model()
+
+def predict(text: str)->dict:
+    vector = tokenizer(text, return_tensors='pt', truncation=True, padding=True)
+    with torch.no_grad():
+        output = model(**vector)
+        logits = output.logits.squeeze()
+        prob = torch.sigmoid(logits).item()
+        toxic = prob > 0.5
+    return {"toxic": toxic, "confidence": prob}
+
+if __name__ == "__main__":
+    logits = predict("you are a very nice person")
+    print(logits)
